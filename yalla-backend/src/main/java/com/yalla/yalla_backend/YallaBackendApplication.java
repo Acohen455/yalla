47a068b477b331ec01d857c7e302270d8e95dd53
Acohen455 <aaroncohen455@gmail.com>
2025-03-13 15:29:00 -0700

    Added backend folder

---
+++ yalla-backend/src/main/java/com/yalla/yalla_backend/YallaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yalla.yalla_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YallaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YallaBackendApplication.class, args);
+	}
+
+}
---
+++ yalla-backend/src/test/java/com/yalla/yalla_backend/YallaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yalla.yalla_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YallaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
