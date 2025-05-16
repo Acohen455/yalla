62db4cf9031382a909d956cf056534bb8820679e
Acohen455 <aaroncohen455@gmail.com>
2025-05-15 21:43:09 -0700

    updates

--- yalla-backend/src/main/java/com/yalla/yalla_backend/interfaces/AppUserDetails.java
+++ yalla-backend/src/main/java/com/yalla/yalla_backend/interfaces/AppUserDetails.java
@@ -6,5 +6,6 @@ import java.util.UUID;
 
 public interface AppUserDetails extends UserDetails {
     UUID getUserId(); //this will fetch either the user OR vendor id
-    String getRole(); //this grabs whether user or vendor
+    String getRole(); //this grabs whether user, vendor, or admin
+    //NOTE: ADMINS ARE GOING TO BE A SUBSET OF USERS
 }
